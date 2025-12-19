ce8d83bdf5d2bf07bca44b81ac6eb93628ab38dc
henokali1 <henokali1@gmail.com>
2025-12-19 13:45:55 +0400

    pwa

--- env_tls/urls.py
+++ env_tls/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from credential_manager.views import dashboard
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
@@ -11,4 +13,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('work-logs/', include('work_log.urls')),
     path('phone-extensions/', include('phone_extension.urls')),
+    path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json'),
+    path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/javascript'), name='sw_js'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
